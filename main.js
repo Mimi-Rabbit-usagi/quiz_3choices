faa12f6e648df45233c8f900eb5644a34a5480ff
RabbitMimi <kitto.ringo@gmail.com>
2024-10-09 22:00:23 +0900

    実装

--- main.js
+++ main.js
@@ -19,8 +19,22 @@
     });
     const li1 = document.createElement("li");
     li1.textContent = quiz[2];
+    li1.addEventListener("click", () => {
+      if (quiz[4] === 1) {
+        li1.classList.add("correct");
+      } else {
+        li1.classList.add("wrong");
+      }
+    });
     const li2 = document.createElement("li");
     li2.textContent = quiz[3];
+    li2.addEventListener("click", () => {
+      if (quiz[4] === 2) {
+        li2.classList.add("correct");
+      } else {
+        li2.classList.add("wrong");
+      }
+    });
 
     ul.appendChild(li0);
     ul.appendChild(li1);
@@ -32,7 +46,13 @@
     main.appendChild(section);
   }
   //問題文、選択肢、選択肢、選択肢、正解（０、１、２）
-  const quiz = ["1の正解は？", "選択肢 A", "選択肢 B", "選択肢 C", 0];
+  const quizzes = [
+    ["1の正解は？", "選択肢 A", "選択肢 B", "選択肢 C", 0],
+    ["2の正解は？", "選択肢 A", "選択肢 B", "選択肢 C", 2],
+    ["3の正解は？", "選択肢 A", "選択肢 B", "選択肢 C", 1],
+  ];
 
-  render(quiz);
+  quizzes.forEach((quiz) => {
+    render(quiz);
+  });
 }
